4abeb548848f29a299f556b7c7765df716dcf924
Aram Sodhi <93051510+aramsodhi@users.noreply.github.com>
2024-02-29 10:33:16 -0800

    Start on collapsing functionality

--- dual_cart.js
+++ dual_cart.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const regular_collapse_button = document.getElementById("regular-collapse");
+const memo_collapse_button = document.getElementById("memo-collapse");
+
+const regular_cart_div = document.querySelector(".regular-cart");
+const memo_cart_div = document.querySelector(".memo-cart");
+
+let regular_cart_collapsed = false;
+let memo_cart_collapsed = false;
+
+regular_collapse_button.addEventListener("click", () => {
+    if (regular_cart_collapsed) {
+        regular_cart_div.style = "height: 50vh";
+        regular_cart_collapsed = false;
+    } else {
+        regular_cart_div.style = "height: 10vh";
+        regular_cart_collapsed = true;
+    }
+});
+
+
+memo_collapse_button.addEventListener("click", () => {
+    if (memo_cart_collapsed) {
+        memo_cart_div.style = "height: 50vh";
+        memo_cart_collapsed = false;
+    } else {
+        memo_cart_div.style = "height: 10vh";
+        memo_cart_collapsed = true;
+    }
+});
